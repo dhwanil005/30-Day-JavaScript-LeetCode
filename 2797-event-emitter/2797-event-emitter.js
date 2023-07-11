bb5b2250fc9d61943af199bcaa021bd94810b718
Dhwanil Shah <55379353+dhwanil005@users.noreply.github.com>
2023-07-11 15:37:39 -0700

    Time: 57 ms (77%), Space: 43.3 MB (19.02%) - LeetHub

--- 2797-event-emitter/2797-event-emitter.js
+++ 2797-event-emitter/2797-event-emitter.js
@@ -5,31 +5,29 @@ class EventEmitter {
   }
 
   subscribe(eventName, callback) {
-    if (!this.subscriptions.has(eventName)) {
-      this.subscriptions.set(eventName, []);
+    let eventSubscriptions = this.subscriptions.get(eventName);
+    if (!eventSubscriptions) {
+      eventSubscriptions = new Set();
+      this.subscriptions.set(eventName, eventSubscriptions);
     }
-    const eventSubscriptions = this.subscriptions.get(eventName);
     const subscription = {
       callback,
       order: this.order++
     };
-    eventSubscriptions.push(subscription);
+    eventSubscriptions.add(subscription);
 
     const unsubscribe = () => {
-      const index = eventSubscriptions.indexOf(subscription);
-      if (index !== -1) {
-        eventSubscriptions.splice(index, 1);
-      }
+      eventSubscriptions.delete(subscription);
     };
 
     return { unsubscribe };
   }
 
   emit(eventName, args = []) {
-    if (!this.subscriptions.has(eventName)) {
+    const eventSubscriptions = this.subscriptions.get(eventName);
+    if (!eventSubscriptions) {
       return [];
     }
-    const eventSubscriptions = this.subscriptions.get(eventName);
     const results = [];
     for (const subscription of eventSubscriptions) {
       results.push(subscription.callback(...args));
